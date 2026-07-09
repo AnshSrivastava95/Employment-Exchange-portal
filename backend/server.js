f5b09117a5d2868be28092940b10cea7733c2c4f
ansh <anshsrivastava112006@gmail.com>
2026-07-09 15:36:28 +0530

    cor

--- backend/server.js
+++ backend/server.js
@@ -7,9 +7,6 @@ const { spawn } = require('child_process');
 
 const app = express();
 
-app.use(cors());
-app.use(express.json());
-
 app.use(cors({
   origin: '*',
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
