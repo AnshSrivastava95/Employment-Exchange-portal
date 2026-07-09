275cfcb1dbea637b9c4a2c858e12ac2d41ab4669
ansh <anshsrivastava112006@gmail.com>
2026-07-09 15:21:04 +0530

    cors

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: '*',
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
